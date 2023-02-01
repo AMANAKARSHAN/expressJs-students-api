a18c324b1f28f6420b5816290e893e07500bca57
Aman <amanakarshan.sinha@primathon.in>
2023-02-02 02:38:50 +0530

    project setup

---
+++ index.js
@@ -0,0 +1,15 @@
+
+import express from 'express' //for es6 and above, if use this then change index.js to index.mjs or add  "type": "module" in package.json
+//const express = require('express')  // for older version
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+const server = app.listen(8000,()=> {
+    const port = server.address().port;
+    const host = server.address().address
+    console.log(`Hello app listening at ${host} ${port}`);
+})
